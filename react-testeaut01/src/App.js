fcc1348c7f1a603501940d7510b5fd9da26e6d58
Dagmar87 <jdfssobrinho@hotmail.com>
2022-10-03 16:11:24 -0300

    criação do componente exemplo

--- react-testeaut01/src/App.js
+++ react-testeaut01/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fakeDatabase = {
+  Dagmar: [
+    "8h00 - Assistir as aulas de Matematica na escola",
+    "12h30 - Encontrar Renata para o almoço",
+    "15h00 - Encontrar Fabio no Clube de Futebol",
+    "20h00 - Jantar à beira da piscina com Pedro e Lorena",
+  ],
+  Inaiana: [
+    "8h00 - Reunião com o CREA",
+    "15h00 - Encontrar Gabriel no Clube de Arquitetura",
+  ],
+};
+
+class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string,
+  };
+
+  render() {
+    const { isLoggedIn, username } = this.props;
+
+    return (
+      <div className="App">
+        {
+          isLoggedIn
+            ? <Home username={username} tasks={fakeDatabase[username]} />
+            : <p>Olá, visitante. Faça login para continuar.</p>
+        }
+      </div>      
+    );
+  }
 }
 
 export default App;
